7725c99f1392e1b9208cea3eaeb8e786dc4b33ea
Thiago Beleren <82111604+ThiagoBeleren@users.noreply.github.com>
2022-07-18 17:51:53 -0300

    Add files via upload

--- configs.py
+++ configs.py
@@ -1,3 +1,3 @@
 def Token():
-    return ' '
+    return ''
 
--- len.py
+++ len.py
@@ -1,23 +1,22 @@
 import discord
 from discord.ext import commands
+import os
 import asyncio
 import random
 import configs
 
-intents = discord.Intents.default()
-intents.members = True
+#intents.members = True
 token = configs.Token()
-bot = commands.Bot(intents=intents, command_prefix="!")
-prefixo = "!"
-botimage = "https://s2.glbimg.com/g5ZxDqRORBmMoW2h1WPkHdT9pCs=/0x0:1465x916/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/c/1/nGOd5lQbOCz8BbFU3QXA/paimon-genshin.jpg"
-command_channel = 923236110744830032
-        
+bot = commands.Bot(intents= discord.Intents.all(), command_prefix="-")
+prefixo = "-"
+
+
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity = discord.Game(name = 'Project Sekai: Colourfull Stage'))
+    await bot.change_presence(activity = discord.Game(name = 'Project Sekai: Colourful Stage'))
     print(bot.user.name, 'está Online')
 
-    
+
 @bot.event
 async def on_member_join(member):
     guild = member.guild #guild = server
@@ -29,94 +28,79 @@ async def on_member_join(member):
     embed3.add_field(name='ID', value=member.id, inline=False)
     embed3.set_image(url="https://cdn.discordapp.com/attachments/996151321096892636/997995707615162388/tenor.gif")
     await welcome_channel.send(embed=embed3)
+    print(f'{member.mention}, entrou no servidor')
 
 
 #Verifica se o canal eh realmente o de comando no momento em que o member mandar a mensagem!
 @bot.event
 async def on_command(ctx, member: discord.Member = None):
-    message_channel_id = bot.get_channel(' ')
-    command_channel = bot.get_channel(923236110744830032)
-    if message_channel_id == command_channel:
-        return
-    elif message_channel_id != command_channel:
+    channel_id = ctx.channel.id
+    print(f"o {ctx.author.mention}, digitou no canal certo! ({channel_id})")
+    
+    if channel_id != 923236110744830032:
         embed = discord.Embed(description=f"Voce nao pode mandar comandos por aqui {ctx.author.mention}")
         await ctx.send(embed=embed)
-        asyncio.sleep(10)
-        await message_channel_id.delete(ctx.author, ctx.message)
+        print(f'{ctx.author.mention} digitou o comando no canal errado')
+        #delete message
         
+    elif channel_id == 923236110744830032:
+        return ' '
+
         
 @bot.command()
 async def twitch(ctx):
     channel_live = bot.get_channel(988883848060354620)
     allowed_mentions = discord.AllowedMentions(everyone = True)
-    admin = bot.get_user(911000560109514752)
-    message = ctx.channel.id
     
     command_channel = bot.get_channel(923236110744830032)
     print(f"{ctx.author}, {command_channel}, {channel_live}")
     
-    if ctx.author == admin and ctx.channel == command_channel:
+    if ctx.author.is_owner():
         print(f"{ctx.author}, {command_channel}, {channel_live}")
-        embed = discord.Embed(descripition=f"ESTOU EM LIVE!")
+        embed = discord.Embed(description=f"ESTOU EM LIVE!")
         embed.add_field(name="Venha ver agora!", value="https://www.twitch.tv/ezdeterno", inline="False")
         embed.add_field(name="Quer ter sua mensagem vista na live? mande-a pelo pix!", value="Chave: thiagobeleren@gmail.com")
         embed.set_image(url='https://i.pinimg.com/564x/b7/c6/e5/b7c6e50a80b89476537711767aa677dd.jpg')
         await channel_live.send(embed=embed, content = '@everyone', allowed_mentions = allowed_mentions)
         print(f'mensagem de live enviada ao canal {channel_live}')
         
+        
 #Show random images or gifs to a embed !working on!
+'''
 @bot.command()
-async def musica(ctx):
-    
-    playlist = random.randint(1, 2)
-    if playlist == 1:
-        embed1 = discord.Embed(
-        description=f'Album recomendado de hoje!'
-        )
-        embed1.add_field(name='Title',
-                        value=f'Beyondcore Evangelix')
-        await ctx.message.send(embed=embed1)
-        await ctx.message.send("https://open.spotify.com/album/4YMCeV11IVZR4G5KJIoZTJ?si=ff6db3bfbfbd43c2")
+async def images(ctx):
+    '''
 
-    elif playlist == 2:
-        embed2 = discord.Embed(
-            description=f'Album recomendado de hoje!'
-        )
-        embed2.add_field(name='Title',
-                            value=f'Encore Emotion Vocal Pop: 02')
-        await ctx.send(embed=embed2)
-        await ctx.send("https://open.spotify.com/album/4YMCeV11IVZR4G5KJIoZTJ?si=ff6db3bfbfbd43c2")
-
-
-#Nao esta funcionando! nao sei pq?, isso iria mostrar oque o user mencionado estava ouvindo!
 @bot.command(aliases=['whois', 'user'])
-async def userinfo(ctx, member: discord.Member = None):
+async def status(ctx, member: discord.Member = None):
     if member:
         if not member.id in [923236110744830032, 988887114634629130]:
             if member.activity == None:
                 activity = 'Inactive'
 
             elif type(member.activity) == discord.Spotify:
-                activity = 'Spotify' \
-                           f'\n{member.activity.artist} - {member.activity.title}' \
-                           f'\n aus [{member.activity.album}]({member.activity.album_cover_url})'
-
+                activity = 'Spotify'
+                           
             else:
                 activity = f'{member.activity.name}'
 
-            embed = discord.Embed(color=member.top_role.color.value, title=f'Musica que {member} esta ouvindo :headphones:')
-            embed.add_field(name='**Activity**', value=f'{activity}', inline=True)
+            embed = discord.Embed(color=member.top_role.color.value, title=f'Atividade que {member} esta fazendo :headphones:', url=member.activity.track_url)
+            embed.add_field(name="**Nome da Musica**", value=member.activity.title)
+            embed.add_field(name='**Activity**', value={activity}, inline=True)
+            embed.set_image(url=member.activity.album_cover_url)
+            embed.add_field(name="**Album**", value=member.activity.album, inline=False)
+            embed.add_field(name='**Artista(s)**', value=member.activity.artist, inline=False)
+            
             await ctx.send(embed=embed)
 
 
 @bot.command()
 async def regras(ctx):
     command_channel = bot.get_channel(923236110744830032)
-    membro = bot.get_user(' ')
     if ctx.channel.id == 923236110744830032:
         embed4 = discord.Embed(title=f'Regras de conduta no servidor', color=10181046)
-        embed4.set_author(name=bot.user.name, icon_url=botimage)
-        embed4.set_image(url="https://images2.alphacoders.com/117/1171852.png")
+        embed4.set_author(name=bot.user.name, icon_url=bot.user.avatar.url)
+        embed4.set_thumbnail(url=bot.user.avatar.url)
         embed4.add_field(name=":one:", value="E proibido qualquer tipo descriminacao e apologia que viole os direitos humanos", inline=False)
         embed4.add_field(name=":two:", value="Qualquer projeto criado deve ter seus devidos creditos dados ao usa-lo", inline=False)
         embed4.add_field(name=":three:", value="Nao e permitido o spam de mensagem nos canais de texto e em qualquer usuario deste servidor", inline=False)
@@ -130,13 +114,13 @@ async def ajuda(ctx):
     if ctx.channel.id == 923236110744830032:
         embed1 = discord.Embed(title="Alguns comandos usados por mim", url="", description="", color=0x109319)
         embed1.set_author(name=bot.user.name, url="")
-        embed1.set_thumbnail(url=botimage)
-        embed1.add_field(name="-" + "music" " nome da musica", value=f"para o bot tocar a musica {bot_de_musica.mention}", inline=False)
-        embed1.add_field(name="!" + "rpg", value="Informacoes sobre o rpg", inline=True)
-        embed1.add_field(name="!" + "flip", value="Tirar cara ou coroa", inline=False)
-        embed1.add_field(name="!" + "regras", value="Ler as regras do servidor")
-        embed1.add_field(name="!" + "sobre", value="sobre o bot e talz")
-        embed1.add_field(name="!"+ "userinfo" "nomedouser", value="mostra os status do spotfy do user mencionado")
+        embed1.set_thumbnail(url=bot.user.avatar.url)
+        embed1.add_field(name=prefixo + "play" " nomedamusica", value=f"para o bot tocar a musica {bot_de_musica.mention}", inline=False)
+        embed1.add_field(name=prefixo + "rpg", value="Informacoes sobre o rpg", inline=False)
+        embed1.add_field(name=prefixo + "flip", value="Tirar cara ou coroa", inline=False)
+        embed1.add_field(name=prefixo + "regras", value="Ler as regras do servidor", inline=False)
+        embed1.add_field(name=prefixo + "sobre", value="sobre o bot e talz", inline=False)
+        embed1.add_field(name=prefixo + "status" " nomedouser", value="mostra os status do user mencionado", inline="false")
         #embed1.add_field(name=prefixo + "roles", value="Para pegar algum cargo!")
         await ctx.channel.send(embed=embed1)
 
@@ -158,13 +142,9 @@ async def flip(ctx):
         elif moeda == 2:
             moeda = 'coroa'
 
-        await channel.send('A face da moeda eh {} {.author}!, :partying_face:'.format(moeda, msg))
+        await channel.send(f'A face da moeda eh {moeda} {ctx.author.mention}! :partying_face:')
 
         print(moeda)
-
-    elif ctx.channel.id != 923236110744830032:
-        await ctx.channel.send(
-            'Voce nao tem permissao para digitar comando aqui bobinho! :stuck_out_tongue_closed_eyes:')
 
 
 @bot.command()
@@ -172,17 +152,14 @@ async def sobre(ctx):
     if ctx.channel.id == 923236110744830032:
         autor = bot.get_user(911000560109514752)
         embed = discord.Embed(
-            title="Sobre mim", description="Entusiasta de bots, desenvolvedor back-end das linguagens Python e Javascript.\n"
-                                            "Estou sempre disposto a ouvir sugestoes :man_mage:, meu discord esta no link acima :wink:",
-            url="https://discord.gg/ZtDpVNzq94"
+            title="Sobre mim", description="Entusiasta de bots, desenvolvedor back-end da linguagem Python e bibliotecas py-cord e tkinter.\n"
+                                            "Estou sempre disposto a ouvir sugestoes :man_mage:, meus contatos esta no link abaixo :wink:",
+            url="https://thiagobeleren.github.io/mylinktree/"
             )
         embed.add_field(name="Sobre o Bot", value=f"O bot e de uso livre, criador por {autor.mention}")
-        embed.set_thumbnail(url="https://s2.glbimg.com/g5ZxDqRORBmMoW2h1WPkHdT9pCs=/0x0:1465x916/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/c/1/nGOd5lQbOCz8BbFU3QXA/paimon-genshin.jpg")
+        embed.set_thumbnail(url=bot.user.avatar.url)
         await ctx.channel.send(embed=embed)
 
-        
-    
-    
     
     
 
