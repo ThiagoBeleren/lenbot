778bf4e6f8d938e72906cd949fb1569dbb843e4a
Thiago Beleren <82111604+ThiagoBeleren@users.noreply.github.com>
2023-02-16 15:17:19 +0000

    mudei somente o usermoderation

--- cogs/userModeration.py
+++ cogs/userModeration.py
@@ -11,18 +11,23 @@ class userModeration(commands.Cog):
         self.bot = bot
         self.date = datetime.now()
         self.hour = 2
-        
+
+      
     @commands.Cog.listener()
     async def on_ready(self):
-        db = sqlite3.connect("warning.sqlite")
-        cursor = db.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS userwarning(user, guild, date, hour, time, reason)")
+        db = sqlite3.connect("warnings.sqlite")
+        cursorwarn = db.cursor()
+        cursorwarn.execute("CREATE TABLE IF NOT EXISTS userwarnings(user, guild, date, hour, time, reason)")
         
-        db2 = sqlite3.connect("userlist.sqlite")
-        cursor2 = db2.cursor()
-        cursor2.execute("CREATE TABLE IF NOT EXISTS userlist(user, guild, date, hour, banned kicked by, reason)")
-        
-        print(f"SQL server READY!")
+        db2 = sqlite3.connect("kicks.sqlite")
+        cursorkick = db2.cursor()
+        cursorkick.execute("CREATE TABLE IF NOT EXISTS userkicks(user, guild, date, hour, kicked by, reason)")
+
+        dbban = sqlite3.connect("bans.sqlite")
+        cursorban = dbban.cursor()
+        cursorban.execute("CREATE TABLE IF NOT EXISTS userbans(user, guild, date, hour, banned by, reason)")
+      
+        print("SQL server READY!")
     
     async def addembed(self, ctx, member: discord.Member, reason, type):
         embed = discord.Embed(title=f"Um membro foi {type} deste servidor! 😔")
@@ -31,22 +36,14 @@ class userModeration(commands.Cog):
         embed.set_footer(text=f"{type} por {ctx.author} \n"
                          f"em _{self.date}_ as {self.hour}")
         await ctx.send(embed=embed)
-        
-    async def addwarn(self, ctx, member, reason, time):
+    
+    async def addwarn(self, ctx, member, reason, time, warnings):
         await self.addembed(ctx, member, reason, type = 'advertido')
         db = sqlite3.connect("warning.sqlite")
         cursor = db.cursor()
-        cursor.execute("INSERT INTO userwarning(user, guild, date, hour, time, reason) VALUES (?, ?, ?, ?, ?, ?)", (member.id, ctx.guild.id, self.date, self.hour, time, reason))
-        db.commit()
-    
-    async def addkick(self, ctx, member: discord.Member, reason: str, time: int):
-        await member.kick(reason=reason)
-        await self.addembed(ctx, member, reason, type = 'expulso')
-        db = sqlite3.connect("userlist.sqlite")
-        cursor = db.cursor()
-        cursor.execute("INSERT INTO userlist(user, guild, date, hour, banned kicked by, reason) VALUES (?, ?, ?, ?, ?, ?)", (member.id, ctx.guild.id, self.date, self.hour, ctx.author.id, reason))
-        
-        cursor.execute("SELECT * FROM userwarning WHERE user = ? AND guild = ?", (member.id, ctx.guild.id))
+        cursor.execute("INSERT INTO userwarnings(user, guild, warns, date, hour, time, reason) VALUES (?, ?, ?, ?, ?, ?)", (member.id, ctx.guild.id, self.date, self.hour, time, reason))
+
+        cursor.execute("SELECT warnings FROM userwarnings WHERE user = ? AND guild = ?", (member.id, ctx.guild.id))
         data = cursor.fetchall()
         
         if len(data) >= 3:
@@ -58,35 +55,58 @@ class userModeration(commands.Cog):
             await ctx.respond(f"{member.mention} has been umuted")
         
         db.commit()
-        
-        
-    @commands.slash_command(description="Expulse um membro do servidor")
-    @commands.has_permissions(kick_members=True)
-    async def kickuser(self, ctx, member: discord.Member, reason: str):
-        await self.addkick(ctx, member, reason)
-        
-        
-    @commands.slash_command()
-    @commands.has_permissions(manage_roles=True)
-    async def warnuser(self, ctx, member: discord.Member, reason: str, time: int):
-        await self.addwarn(ctx, member, reason, time)
-        
-        
-    @commands.slash_command()
-    @commands.has_permissions(kick_members=True, manage_roles=True)
-    async def removewarn(self, ctx, member: discord.Member):
+
+    async def addkick(self, ctx, member: discord.Member, reason: str):
+        await member.kick(reason=reason)
+        await self.addembed(ctx, member, reason, type = 'expulso')
+        db = sqlite3.connect("userlist.sqlite")
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO userkicks(user, guild, date, hour, kicked by, reason) VALUES (?, ?, ?, ?, ?, ?)", (member.id, ctx.guild.id, self.date, self.hour, ctx.author.id, reason))
+        db.commit()
+      
+    async def addban(self, ctx, member: discord.Member, reason: str):
+        await member.ban(reason=reason)
+        db = sqlite3.connect("userbans.sqlite3")
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO userbans(user, guild, date, hour, banned by, reason) VALUES (?, ?, ?, ?. ?, ?)", (member.id, ctx.guild.id, self.date, self.hour, ctx.author.id, reason))
+        db.commit()
+
+    async def removewarn(seld, ctx, member: discord.Member):
         db = sqlite3.connect("userinfo.sqlite")
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM userwarning WHERE user = ? AND guild = ?", (member.id, ctx.guild.id))
+        cursor.execute("SELECT * FROM userwarnings WHERE user = ? AND guild = ?", (member.id, ctx.guild.id))
         data = cursor.fetchone()
         if data:
-            cursor.execute("DELETE FROM userwarning WHERE user = ? AND guild = ?", (member.id, ctx.guild.id))
+            cursor.execute("DELETE FROM userwarnings WHERE user = ? AND guild = ?", (member.id, ctx.guild.id))
             await ctx.respond(f"Advertencias de {member.mention} foram removidos!")
         
         else:
             await ctx.respond(f"{member.mention} nao tem advertencias!")
             
         db.commit()
+
+    @commands.slash_command(description="Expulse um membro ")
+    @commands.has_permissions(kick_members=True)
+    async def kickuser(self, ctx, member: discord.Member, reason: str):
+        await self.addkick(ctx, member, reason)
+
+      
+    @commands.slash_command(description="Mande uma advertencia a um membro")
+    @commands.has_permissions(manage_roles=True)
+    async def warnuser(self, ctx, member: discord.Member, reason: str, time: int, warnings: int):
+        await self.addwarn(ctx, member, reason, time, warnings)
+
+      
+    @commands.slash_command(description="Bana um membro")
+    @commands.has_permissions(ban_members=True)
+    async def banuser(self, ctx, member: discord.Member, reason: str):
+      await self.addban(ctx, member, reason)
+
+
+    @commands.slash_command(description="Remova uma advertecia de um membro")
+    @commands.has_permissions(kick_members=True, manage_roles=True)
+    async def removewarnuser(self, ctx, member: discord.Member):
+        await self.removewarn(ctx, member)
         
 def setup(bot):
     bot.add_cog(userModeration(bot))
