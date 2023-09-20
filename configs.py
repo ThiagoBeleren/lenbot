71059ee00d1a15f06eb2313728d4efd543e4773f
Thiago Beleren <82111604+ThiagoBeleren@users.noreply.github.com>
2023-09-20 17:53:18 -0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,8 +3,8 @@ def Token():
 
 
 def SpotifyClientID():
-  return '5352dadbfee1488b9eb833e9c071c1b6'
+  return ''
 
 
 def SpotifyClientSecret():
-  return '38f8d1a26d484b148093d9af9a6fc70a'
+  return ''
