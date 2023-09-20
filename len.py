45311e61cf149b1280de582ec792a562c86ff702
Thiago Beleren <82111604+ThiagoBeleren@users.noreply.github.com>
2023-09-20 17:54:50 -0300

    updated main
    colorama added to colorize terminal

--- len.py
+++ len.py
@@ -1,8 +1,11 @@
 import discord
-from discord.ext import commands
 import os
 import platform
 import configs
+
+from discord.ext import commands
+from colorama import Fore
+
 
 intents = discord.Intents.all()
 intents.members = True
@@ -11,19 +14,15 @@ bot = commands.Bot(intents=intents, permissions=8, command_prefix="/")
 
 @bot.event
 async def on_ready():
-  await bot.change_presence(activity=discord.Game(name='Project Sekai: Colourfull Stage'))
-  print(f"{bot.user.name} esta online!")
-  print(f"Bot identifier: {str(bot.user.id)}")
-  print(f"Discord Version: {str(discord.__version__)}")
-  print(f"Python Version: {str(platform.python_version())}")
+    await bot.change_presence(activity=discord.Game(name='Project Sekai: Colourfull Stage'))
+    print(f"{Fore.BLUE} {bot.user.name} esta online!")
+    print(f"{Fore.BLUE} Bot identifier: {str(bot.user.id)}")
+    print(f"{Fore.BLUE} Discord Version: {str(discord.__version__)}")
+    print(f"{Fore.BLUE} Python Version: {str(platform.python_version())}")
 
-#Cogs / Modules
-
+# Cogs / Modules
 for filename in os.listdir("./cogs"):
-  if filename.endswith(".py"):
-    bot.load_extension(f"cogs.{filename[:-3]}")
-
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
 
 bot.run(configs.Token())
-
-
