bc8de0b34e327301007ae8bba7d4797990b56a66
Thiago Beleren <82111604+ThiagoBeleren@users.noreply.github.com>
2023-09-21 15:05:29 -0300

    migrated to 2.5.0 nextcord

---
+++ cogs/botmoderation.py
@@ -0,0 +1,19 @@
+import nextcord
+import time
+import sqlite3
+
+from nextcord.ext import commands
+from colorama import Fore
+
+
+class BotModeration(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{Fore.YELLOW}`Bot Moderation cog` is ready!")
+
+
+def setup(bot):
+    bot.add_cog(BotModeration(bot))
---
+++ cogs/events.py
@@ -0,0 +1,142 @@
+import nextcord
+
+from colorama import Fore
+from nextcord.ext import commands
+
+
+class Events(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{Fore.YELLOW}`Events cog` is ready!')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        guild = member.guild  # guild = server
+        welcome_channel = member.guild.system_channel
+        guildsids = [1135982907043889192]
+
+        if guild.id == guildsids[0] in guildsids:  # Server jogadinhas
+            embed = nextcord.Embed(
+                description=f":partying_face: Bem vindo ao servidor, {member.mention}. \n"
+                            f"Espero que se divirta por aqui. \n"
+                            f"Você é o **{len(guild.members)}** participante do servidor. \n")
+            embed.set_author(name=member.display_name, icon_url=member.display_avatar)
+            embed.set_footer(text="ID \n"
+                                  f"{member.id}")
+            embed.set_image(
+                url=
+                "https://cdn.discordapp.com/attachments/1135982909237493853/1135990644536512562/tenor.gif"
+            )
+            await welcome_channel.send(embed=embed)
+            print(f'{member.mention}, entrou no servidor')
+
+        else:
+            embed = nextcord.Embed(
+                description=f":partying_face: Bem vindo ao servidor, {member.mention}. \n"
+                            f"Espero que se divirta por aqui. \n"
+                            f"Você é o **{len(guild.members)}** participante do servidor. \n")
+            embed.set_author(name=member.display_name, icon_url=member.display_avatar)
+            embed.set_footer(text="ID \n"
+                                  f"{member.id}")
+            embed.set_image(
+                url=
+                "https://cdn.discordapp.com/attachments/996151321096892636/997995707615162388/tenor.gif"
+            )
+            await welcome_channel.send(embed=embed)
+            print(f'{member.mention}, entrou no servidor')
+
+    @commands.Cog.listener()
+    @commands.has_permissions(administrator=True)
+    async def on_raw_reaction_add(self, payload):
+        member = payload.member
+        guild = member.guild
+        emoji = payload.emoji.name
+        role_message_id = 999072734954921994  # server Melhores De Piltover
+        role_message_id2 = 0  # server jogadinhas
+
+        if role_message_id == payload.message_id:
+
+            if emoji == '💻':
+                role = nextcord.utils.get(guild.roles, name="Programadores 💻")
+
+            if emoji == '👑':
+                role = nextcord.utils.get(guild.roles, name="Anime_Fan 👑")
+
+            if emoji == '🎶':
+                role = nextcord.utils.get(guild.roles, name="Just_Chill 🎶")
+
+            if emoji == "🏠":
+                role = nextcord.utils.get(guild.roles, name="Rioters 🏠")
+
+            if emoji == "🏆":
+                role = nextcord.utils.get(guild.roles, name="Indie 🏆")
+
+            if emoji == '🎵':
+                role = nextcord.utils.get(guild.roles, name="Rhythm_Games 🎵")
+
+            if emoji == '🕹️':
+                role = nextcord.utils.get(guild.roles, name='Casual 🕹️')
+
+            if emoji == '⚔️':
+                role = nextcord.utils.get(guild.roles, name='Competitivo ⚔️')
+
+            if emoji == '🔫':
+                role = nextcord.utils.get(guild.roles, name="FPS 🔫")
+
+            if emoji == '🎲':
+                role = nextcord.utils.get(guild.roles, name="RPG 🎲")
+
+        await member.add_roles(role)
+
+    @commands.Cog.listener()
+    @commands.has_permissions(administrator=True)
+    async def on_raw_reaction_remove(self, payload):
+        role_message_id = 999072734954921994
+        if role_message_id == payload.message_id:
+            guild = await (self.bot.fetch_guild(payload.guild_id))
+            emoji = payload.emoji.name
+
+            if emoji == '💻':
+                role = nextcord.utils.get(guild.roles, name="Programadores 💻")
+
+            if emoji == '👑':
+                role = nextcord.utils.get(guild.roles, name="Anime_Fan 👑")
+
+            if emoji == '🎶':
+                role = nextcord.utils.get(guild.roles, name="Just_Chill 🎶")
+
+            if emoji == "🏠":
+                role = nextcord.utils.get(guild.roles, name="Rioters 🏠")
+
+            if emoji == "🏆":
+                role = nextcord.utils.get(guild.roles, name="Indie 🏆")
+
+            if emoji == '🎵':
+                role = nextcord.utils.get(guild.roles, name="Rhythm_Games 🎵")
+
+            if emoji == '🕹️':
+                role = nextcord.utils.get(guild.roles, name='Casual 🕹️')
+
+            if emoji == '⚔️':
+                role = nextcord.utils.get(guild.roles, name='Competitivo ⚔️')
+
+            if emoji == '🔫':
+                role = nextcord.utils.get(guild.roles, name="FPS 🔫")
+
+            if emoji == '🎲':
+                role = nextcord.utils.get(guild.roles, name="RPG 🎲")
+
+            member = await (guild.fetch_member(payload.user_id))
+
+            if member is not None:
+                await member.remove_roles(role)
+            else:
+                print('Usuario Nao encontrado! :(')
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
